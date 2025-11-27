035fbb48bbb2d9e03512bdaa928709d148fd29b1
Yoonj310 <opi2982@gmail.com>
2025-11-28 02:20:36 +0900

    [MARKUP] Tag 컴포넌트 퍼블리싱 #39

--- src/app/(with-header)/user/likes/page.tsx
+++ src/app/(with-header)/user/likes/page.tsx
@@ -1,9 +1,11 @@
 import SearchBar from "@/components/common/SearchBar";
+import Tag from "@/components/common/Tag";
 
 export default function Page() {
   return (
     <div className="flex flex-col h-full px-10 justify-start items-center gap-5">
       <SearchBar></SearchBar>
+      <Tag label={"뚜벅이"}></Tag>
     </div>
   )
 }
