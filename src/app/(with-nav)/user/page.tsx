09643bc5767f5e801c5cf7a6195f53a73f4835e4
Yoonj310 <opi2982@gmail.com>
2025-11-27 13:46:40 +0900

    [MARKUP] UserCourseList 컴포넌트 퍼블리싱 #25

--- src/app/(with-nav)/user/page.tsx
+++ src/app/(with-nav)/user/page.tsx
@@ -1,7 +1,42 @@
+"use client";
+
+import { FileText, Heart } from "lucide-react";
+import PrimaryButton from "@/components/common/PrimaryButton";
+import UserCourseList from "@/components/user/UserCourseList";
+
 export default function Page() {
+
+  function handleSubmit(e?: React.FormEvent) {
+    if (e) e.preventDefault();
+
+    // 실제 로그인 요청 처리
+    console.log("로그아웃 요청");
+  }
+
   return (
-    <h1 className="text-3xl font-bold">
-      사용자 페이지
-    </h1>
+    <div className="flex flex-col p-10 justify-center items-center gap-5">
+      <div className="w-full flex justify-start items-end gap-3">
+        <span className="text-xl font-bold">닉네임</span>
+        <span className="text-sm font-medium text-gray-200">yoonj310@gmail.com</span>
+      </div>
+      <div className="w-full flex gap-5">
+        <div className="flex-1">
+          <UserCourseList icon={FileText} label="작성글" count={8} href="/user/posts" />
+        </div>
+        <div className="flex-1">
+          <UserCourseList icon={Heart} label="좋아요" count={12} href="/user/likes" />
+        </div>
+      </div>
+
+      <div>
+
+      </div>
+      <div className="flex justify-center mt-2">
+        <PrimaryButton
+          label="로그아웃"
+          onClick={handleSubmit}
+        />
+      </div>
+    </div>
   )
 }
---
+++ src/components/user/UserCourseList.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { LucideIcon } from "lucide-react";
+
+interface UserCourseListProps {
+    icon: LucideIcon;       // 아이콘 컴포넌트
+    label: string;          // 같은 텍스트
+    count: number;          // 갯수
+    href: string;           // 이동할 링크
+}
+
+export default function UserCourseList({ icon: Icon, label, count, href }: UserCourseListProps) {
+    return (
+        <Link href={href}>
+            <div className="w-full flex items-center p-3 rounded-2xl bg-(--color-userCourseList)">
+
+                {/* 왼쪽 (아이콘 + 텍스트) */}
+                <div className="flex flex-1 flex-col items-center justify-center gap-1">
+                    <Icon className="text-xl"  strokeWidth={2} />
+                    <span className="text-xs font-semibold">{label}</span>
+                </div>
+
+                {/* 가운데 세로 막대기 */}
+                <div className="w-px h-10 bg-gray-300"></div>
+
+                {/* 오른쪽 (숫자) */}
+                <div className="flex flex-1 items-center justify-center">
+                    <span className="text-xl font-bold">{count}</span>
+                </div>
+            </div>
+        </Link>
+
+    );
+}
