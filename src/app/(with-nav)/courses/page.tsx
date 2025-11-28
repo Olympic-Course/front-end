098048853e2840120c8d79c478120666b277b75f
Yoonj310 <opi2982@gmail.com>
2025-11-29 01:08:26 +0900

    [MARKUP] MenuSection 컴포넌트 퍼블리싱 #54

--- src/app/(with-nav)/courses/page.tsx
+++ src/app/(with-nav)/courses/page.tsx
@@ -1,7 +1,13 @@
+import MenuSection from "@/components/common/MenuSection";
+
 export default function Page() {
   return (
-    <h1 className="text-3xl font-bold">
-      코스 검색 페이지
-    </h1>
+    <div className="flex flex-col h-full px-5 pt-3 justify-start items-center gap-5">
+      <MenuSection title={"이달의 베스트 3 코스"}>
+        <div className="w-full h-40 bg-amber-300 flex justify-center items-center">
+          이달의 베스트 3코스 영역
+        </div>
+      </MenuSection>
+    </div>
   )
 }
