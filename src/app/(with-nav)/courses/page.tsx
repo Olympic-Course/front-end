275a1dfff56a071d12b485fa18fd2a929f449a49
Yoonj310 <opi2982@gmail.com>
2025-11-29 03:04:11 +0900

    [MARKUP] RecommendedCourseCard 컴포넌트 퍼블리싱 #56

--- src/app/(with-nav)/courses/page.tsx
+++ src/app/(with-nav)/courses/page.tsx
@@ -1,5 +1,6 @@
 import MenuSection from "@/components/common/MenuSection";
 import BestCoursesSection from "@/components/course/BestCoursesSection";
+import RecommendedCourseCard from "@/components/course/RecommendedCourseCard";
 import courseList from "@/mock/courseList.json"
 
 export default function Page() {
@@ -7,6 +8,27 @@ export default function Page() {
     <div className="flex flex-col h-full px-5 pt-3 justify-start items-center gap-5">
       <MenuSection title={"이달의 베스트3 코스"}>
         <BestCoursesSection bestCourses={courseList.bestCourses} />
+      </MenuSection>
+
+      <MenuSection title={"추천 코스"}>
+        <div className="flex w-full gap-3 justify-between">
+          <RecommendedCourseCard
+          courseId={55}
+          thumbnail={""}
+          title={"2NE1 콘서트 코스"}
+          writer={"듀?"}
+          likeNum={12}
+          liked={true}
+        />
+        <RecommendedCourseCard
+          courseId={55}
+          thumbnail={""}
+          title={"2NE1 콘서트 코스"}
+          writer={"듀?"}
+          likeNum={12}
+          liked={true}
+        />
+        </div>
       </MenuSection>
     </div>
   )
