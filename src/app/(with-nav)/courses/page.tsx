fd1eb23cd3b436f42f24418f910b66ea571796d6
Yoonj310 <opi2982@gmail.com>
2025-11-29 02:26:24 +0900

    [MARKUP] BestCourseCard 컴포넌트 퍼블리싱 #53

--- src/app/(with-nav)/courses/page.tsx
+++ src/app/(with-nav)/courses/page.tsx
@@ -1,11 +1,23 @@
 import MenuSection from "@/components/common/MenuSection";
+import BestCourseCard from "@/components/course/BestCourseCard";
+import courseList from "@/mock/courseList.json"
 
 export default function Page() {
   return (
     <div className="flex flex-col h-full px-5 pt-3 justify-start items-center gap-5">
-      <MenuSection title={"이달의 베스트 3 코스"}>
-        <div className="w-full h-40 bg-amber-300 flex justify-center items-center">
-          이달의 베스트 3코스 영역
+      <MenuSection title={"이달의 베스트3 코스"}>
+        <div className="w-full flex justify-between items-center gap-3">
+          {courseList.bestCourses.map((course) => (
+            <BestCourseCard
+              key={course.courseId}
+              courseId={course.courseId}
+              thumbnail={course.thumbnail}
+              title={course.title}
+              writer={course.writer}
+              liked={course.liked}
+              likeNum={course.likeNum}
+            />
+          ))}
         </div>
       </MenuSection>
     </div>
---
+++ src/components/course/BestCourseCard.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+import LikeIcon from "../common/LikeIcon";
+
+interface BestCourseCardProps {
+    courseId: number;
+    thumbnail?: string | null;
+    title: string;
+    writer: string;
+    liked: boolean;
+    likeNum: number
+}
+
+export default function BestCourseCard({ courseId, thumbnail, title, writer, liked, likeNum }: BestCourseCardProps) {
+    const displayThumbnail =
+        thumbnail && thumbnail.trim() !== "" ? thumbnail : "/img/OlCo_logo_3.png";
+
+    return (
+        <div className="flex flex-col w-full flex-1 min-w-0">
+            <Link href={`/courses/${courseId}`} className="w-full">
+                {/* 썸네일 이미지 영역 */}
+                <div className="relative aspect-4/5 border border-[#E1E1E1] rounded-lg overflow-hidden bg-gray-100">
+                    <Image
+                        src={displayThumbnail}
+                        alt="thumbnail"
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+            </Link>
+
+            {/* 게시글 정보 영역 - title, writer, tag, likes */}
+            <div className="flex p-1 items-center justify-between">
+                <div className="flex flex-col flex-1 gap-0.5 min-w-0">
+                    <span className="text-xs font-bold truncate">{title}</span>
+                    <span className="text-[9px] font-semibold text-[#8E8E8E] truncate">{writer}</span>
+                </div>
+
+                {/* 좋아요 영역 */}
+                <LikeIcon liked={liked} count={likeNum} />
+            </div>
+        </div>
+    );
+}
