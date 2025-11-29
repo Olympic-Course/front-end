c3b6d04df6e986c6e6ad349f7a19e6ca5b8fd784
Yoonj310 <opi2982@gmail.com>
2025-11-30 02:19:04 +0900

    [MARKUP] CourseLocationItem 컴포넌트 퍼블리싱 #63

---
+++ src/components/course/CourseLocationItem.tsx
@@ -0,0 +1,30 @@
+import { ChevronDown, ChevronRight } from "lucide-react";
+
+interface CourseLocationItemProps{
+    stepOrder: number;
+    name: string;
+    memoActive: boolean;
+    onClick?: () => void;
+}
+
+export default function CourseLocationItem({ stepOrder, name, memoActive, onClick }: CourseLocationItemProps) {
+    return (
+        <button
+            onClick={onClick}
+            className="flex items-center py-1 pl-1 pr-3 rounded-full border border-[#DBDBDB] justify-between gap-2 w-full"
+        >
+            <div className="flex items-center justify-center w-8 h-8 shrink-0 rounded-full bg-(--color-main)">
+                <span className="font-medium text-xs text-white">{stepOrder}</span>
+            </div>
+
+            <span className="text-sm font-semibold flex-1 text-left">
+                {name}
+            </span>
+
+            {memoActive
+                ? <ChevronDown size={25} color="#828282" strokeWidth={1} />
+                : <ChevronRight size={25} color="#828282" strokeWidth={1} />
+            }
+        </button>
+    );
+}
