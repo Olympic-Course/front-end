eabf8caf8e616d55d12232f3091b7d4816eac90a
Yoonj310 <opi2982@gmail.com>
2025-12-01 02:02:06 +0900

    [MARKUP] 코스 작성 페이지 UI 퍼블리싱 #68

--- src/components/course/CourseLocationItem.tsx
+++ src/components/course/CourseLocationItem.tsx
@@ -1,6 +1,7 @@
 import { ChevronDown, ChevronRight } from "lucide-react";
+import CourseLocationMemo from "./CourseLocationMemo";
 
-interface CourseLocationItemProps{
+interface CourseLocationItemProps {
     stepOrder: number;
     name: string;
     memoActive: boolean;
@@ -9,22 +10,30 @@ interface CourseLocationItemProps{
 
 export default function CourseLocationItem({ stepOrder, name, memoActive, onClick }: CourseLocationItemProps) {
     return (
-        <button
-            onClick={onClick}
-            className="flex items-center py-1 pl-1 pr-3 rounded-full border border-[#DBDBDB] justify-between gap-2 w-full"
-        >
-            <div className="flex items-center justify-center w-8 h-8 shrink-0 rounded-full bg-(--color-main)">
-                <span className="font-medium text-xs text-white">{stepOrder}</span>
-            </div>
+        <div className="flex flex-col w-full gap-1">
+            <button
+                onClick={onClick}
+                className="flex items-center py-1 pl-1 pr-3 rounded-full border border-[#DBDBDB] justify-between gap-2 w-full"
+            >
+                <div className="flex items-center justify-center w-8 h-8 shrink-0 rounded-full bg-(--color-main)">
+                    <span className="font-medium text-xs text-white">{stepOrder}</span>
+                </div>
 
-            <span className="text-sm font-semibold flex-1 text-left">
-                {name}
-            </span>
+                <span className="text-sm font-semibold flex-1 text-left">
+                    {name}
+                </span>
 
-            {memoActive
-                ? <ChevronDown size={25} color="#828282" strokeWidth={1} />
-                : <ChevronRight size={25} color="#828282" strokeWidth={1} />
-            }
-        </button>
+                {memoActive
+                    ? <ChevronDown size={25} color="#828282" strokeWidth={1} />
+                    : <ChevronRight size={25} color="#828282" strokeWidth={1} />
+                }
+            </button>
+            
+            {memoActive && (
+                <CourseLocationMemo
+                    locationMemo={"춥거나 더울때는 소마미술관으로 대피🚨"}
+                />
+            )}
+        </div>
     );
 }
