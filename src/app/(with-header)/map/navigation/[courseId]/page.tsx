d7e140038c1d3908fefc08fdbb6efd55b1c32da3
Yoonj310 <opi2982@gmail.com>
2025-12-03 00:44:56 +0900

    [MARKUP] 코스 따라가기 페이지 UI 퍼블리싱 #92

--- src/app/(with-header)/map/navigation/[courseId]/page.tsx
+++ src/app/(with-header)/map/navigation/[courseId]/page.tsx
@@ -3,6 +3,7 @@
 import BottomModal from "@/components/common/BottomModal";
 import HeaderLayout from "@/components/common/HeaderLayout";
 import MenuSection from "@/components/common/MenuSection";
+import PrimaryButton from "@/components/common/PrimaryButton";
 import CourseSection from "@/components/course/CourseSection";
 import KakaoMap from "@/components/map/KakaoMap";
 import MapFilter from "@/components/map/MapFilter";
@@ -14,8 +15,8 @@ export default function Page() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <HeaderLayout>
-            <div className="flex flex-col h-[calc(100vh-64px)]">
+        <HeaderLayout title="코스 따라가기">
+            <div className="flex flex-col h-[calc(100vh-64px)] relative">
                 <div className="w-full h-full">
                     <KakaoMap />
                 </div>
@@ -26,8 +27,17 @@ export default function Page() {
                 >
                     <div className="flex-1 w-full overflow-y-auto px-5 pb-5 flex flex-col gap-5">
                         <MenuSection title={"듀?님이 걷고있는 코스"}>
-                            <CourseSection stepOrder={1} name={"팔각정2"} />
+                            <div className="flex flex-col gap-2 my-2 w-full">
+                                <CourseSection stepOrder={1} name={"팔각정2"} />
+                                <CourseSection stepOrder={2} name={"팔각정3"} />
+                                <CourseSection stepOrder={3} name={"팔각정4"} />
+                                <CourseSection stepOrder={4} name={"팔각정5"} />
+                            </div>
                         </MenuSection>
+                        <PrimaryButton 
+                        label={"안내 종료"}
+                        onClick={() => router.back()}
+                        />
                     </div>
                 </BottomModal>
             </div>
