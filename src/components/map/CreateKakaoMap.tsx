93ad99b9ab3fd2511f8b5004ad04ccb4b80a9dc8
Yoonj310 <opi2982@gmail.com>
2025-12-05 06:10:20 +0900

    kakao map 관련 eslint 설정 #103

--- src/components/map/CreateKakaoMap.tsx
+++ src/components/map/CreateKakaoMap.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 "use client";
 
 import { Map, MapMarker, useKakaoLoader, CustomOverlayMap } from "react-kakao-maps-sdk";
--- src/components/map/KakaoMap.tsx
+++ src/components/map/KakaoMap.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 "use client";
 
 import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
